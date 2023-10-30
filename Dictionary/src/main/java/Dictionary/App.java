7879df3bd8bb163d149f49dd8d015dd145f982e9
MinhPhuoc3504 <124911496+MinhPhuoc3504@users.noreply.github.com>
2023-10-30 16:16:51 +0700

    Create Base UI

--- Dictionary/src/main/java/Dictionary/App.java
+++ Dictionary/src/main/java/Dictionary/App.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/View/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/View/BaseScene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("CMP_Project");
         stage.setScene(scene);
         stage.show();
     }
